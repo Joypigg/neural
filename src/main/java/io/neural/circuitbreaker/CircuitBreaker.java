3e09ce9ca28b3cb102ce4dcf15f2aa153f45b0ca
lry <lry@sxw.cn>
2018-01-26 09:48:27 +0800

    大重构： 1.重构分布式限流 2.重构服务降级

--- src/main/java/io/neural/circuitbreaker/CircuitBreaker.java
+++ src/main/java/io/neural/circuitbreaker/CircuitBreaker.java
@@ -1,71 +1,128 @@
 package io.neural.circuitbreaker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
- * Circuit Breaker Component.
- * <br>
- * 1.Closed：熔断器关闭状态，调用失败次数积累，到了阈值（或一定比例）则启动熔断机制<br>
- * 2.Open：熔断器打开状态，此时对下游的调用都内部直接返回错误，不走网络，但设计了一个时钟选项，默认的时钟达到了一定时间（这个时间一般设置成平均故障处理时间，也就是MTTR），到了这个时间，进入半熔断状态<br>
- * 3.Half-Open：半熔断状态，允许定量的服务请求，如果调用都成功（或一定比例）则认为恢复了，关闭熔断器，否则认为还没好，又回到熔断器打开状态<br>
- * @param <T> the type of the value monitored by this circuit breaker
+ * The Circuit Breaker
+ *
  * @author lry
  */
-public interface CircuitBreaker<T> {
-	
-	/**
-	 * Returns the current open state of this circuit breaker. A return value of
-	 * <strong>true</strong> means that the circuit breaker is currently open
-	 * indicating a problem in the monitored sub system.
-	 *
-	 * @return the current open state of this circuit breaker
-	 */
-	boolean isOpen();
+public class CircuitBreaker {
 
-	/**
-	 * Returns the current closed state of this circuit breaker. A return value
-	 * of <strong>true</strong> means that the circuit breaker is currently
-	 * closed. This means that everything is okay with the monitored sub system.
-	 *
-	 * @return the current closed state of this circuit breaker
-	 */
-	boolean isClosed();
+    private static final Logger logger = LoggerFactory.getLogger(CircuitBreaker.class);
 
-	/**
-	 * Checks the state of this circuit breaker and changes it if necessary. The
-	 * return value indicates whether the circuit breaker is now in state
-	 * {@code CLOSED}; a value of <strong>true</strong> typically means that the
-	 * current operation can continue.
-	 *
-	 * @return <strong>true</strong> if the circuit breaker is now closed;
-	 *         <strong>false</strong> otherwise
-	 */
-	boolean checkState();
+    private String name;
 
-	/**
-	 * Closes this circuit breaker. Its state is changed to closed. If this
-	 * circuit breaker is already closed, this method has no effect.
-	 */
-	void close();
+    private CircuitBreakerConfig config;
 
-	/**
-	 * Opens this circuit breaker. Its state is changed to open. Depending on a
-	 * concrete implementation, it may close itself again if the monitored sub
-	 * system becomes available. If this circuit breaker is already open, this
-	 * method has no effect.
-	 */
-	void open();
+    private volatile CircuitBreakerState state = CircuitBreakerState.CLOSED;
 
-	/**
-	 * Increments the monitored value and performs a check of the current state
-	 * of this circuit breaker. This method works like {@link #checkState()},
-	 * but the monitored value is incremented before the state check is
-	 * performed.
-	 *
-	 * @param increment
-	 *            value to increment in the monitored value of the circuit
-	 *            breaker
-	 * @return <strong>true</strong> if the circuit breaker is now closed;
-	 *         <strong>false</strong> otherwise
-	 */
-	boolean incrementAndCheckState(T increment);
-	
+    /**
+     * 最近进入open状态的时间
+     */
+    private volatile long lastOpenedTime;
+
+    /**
+     * closed状态下失败次数
+     */
+    private LimitCounter failCount;
+
+    /**
+     * half-open状态的连续成功次数,失败立即清零
+     */
+    private AtomicInteger consecutiveSuccCount = new AtomicInteger(0);
+
+    public CircuitBreaker(String name, CircuitBreakerConfig config) {
+        this.config = config;
+        this.name = name;
+        failCount = new LimitCounter(config.getFailCountWindowInMs(), config.getFailThreshold());
+    }
+
+    public boolean isOpen() {
+        return CircuitBreakerState.OPEN == state;
+    }
+
+    public boolean isHalfOpen() {
+        return CircuitBreakerState.HALF_OPEN == state;
+    }
+
+    public boolean isClosed() {
+        return CircuitBreakerState.CLOSED == state;
+    }
+
+    //状态操作
+
+    /**
+     * closed -> open | half open -> open
+     */
+    public void open() {
+        lastOpenedTime = System.currentTimeMillis();
+        state = CircuitBreakerState.OPEN;
+        logger.debug("circuit open,key:{}", name);
+    }
+
+    /**
+     * open -> half open
+     */
+    public void openHalf() {
+        consecutiveSuccCount.set(0);
+        state = CircuitBreakerState.HALF_OPEN;
+        logger.debug("circuit open-half,key:{}", name);
+    }
+
+    /**
+     * half open -> close
+     */
+    public void close() {
+        failCount.reset();
+        state = CircuitBreakerState.CLOSED;
+        logger.debug("circuit close,key:{}", name);
+    }
+
+    //阈值判断
+
+    /**
+     * 是否应该转到half open
+     * 前提是 open state
+     *
+     * @return
+     */
+    public boolean isOpen2HalfOpenTimeout() {
+        return System.currentTimeMillis() - config.getOpen2HalfOpenTimeoutInMs() > lastOpenedTime;
+    }
+
+    /**
+     * 是否应该从close转到open
+     *
+     * @return
+     */
+    public boolean isCloseFailThresholdReached() {
+        return failCount.thresholdReached();
+    }
+
+    /**
+     * half-open状态下是否达到close的阈值
+     *
+     * @return
+     */
+    public boolean isConsecutiveSuccessThresholdReached() {
+        return consecutiveSuccCount.get() >= config.getConsecutiveSuccThreshold();
+    }
+
+    public void incrFailCount() {
+        int count = failCount.incrAndGet();
+        logger.debug("incr fail count:{},key:{}", count, name);
+    }
+
+    public AtomicInteger getConsecutiveSuccCount() {
+        return consecutiveSuccCount;
+    }
+
+    public CircuitBreakerState getState() {
+        return state;
+    }
+
 }
--- src/test/java/io/neural/circuitbreaker/CircuitBreakerTest.java
+++ src/test/java/io/neural/circuitbreaker/CircuitBreakerTest.java
@@ -1,20 +1,110 @@
 package io.neural.circuitbreaker;
 
+import io.neural.circuitbreaker.exception.CircuitBreakerOpenException;
+import io.neural.circuitbreaker.handler.ProxyFactory;
+import io.neural.circuitbreaker.service.DemoService;
+import io.neural.circuitbreaker.service.DemoServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+
 import java.util.concurrent.TimeUnit;
 
 public class CircuitBreakerTest {
 
-	public static void main(String[] args) {
-		EventCountCircuitBreaker breaker = new EventCountCircuitBreaker(5, 2, TimeUnit.MINUTES, 5, 10, TimeUnit.MINUTES);
-		if (breaker.checkState()) {
-	         try {
-	        	 
-	         } catch (Exception ex) {
-	             breaker.incrementAndCheckState();
-	         }
-	     } else {
-	         // return an error code, use an alternative service, etc.
-	     }
-	}
-	
+    DemoService demoService;
+
+    @Before
+    public void prepare() {
+        demoService = ProxyFactory.proxyBean(new DemoServiceImpl());
+    }
+
+    @Test
+    public void testSucc() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(demoService.getUuid(1));
+        }
+    }
+
+    @Test(expected = CircuitBreakerOpenException.class)
+    public void testOpen() {
+        for (int i = 0; i < 6; i++) {
+            try {
+                demoService.getUuid(2);
+            } catch (Exception e) {
+
+            }
+        }
+        demoService.getUuid(1);
+    }
+
+    @Test
+    public void testFailWindow() throws InterruptedException {
+        for (int i = 0; i < 6; i++) {
+            Thread.sleep(13 * 1000);
+            try {
+                demoService.getUuid(2);
+            } catch (Exception e) {
+
+            }
+        }
+        try {
+            demoService.getUuid(2);
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    public void testHalfOpen2Open() throws InterruptedException {
+        for (int i = 0; i < 6; i++) {
+            try {
+                demoService.getUuid(2);
+            } catch (Exception e) {
+
+            }
+        }
+        //into half open
+        TimeUnit.SECONDS.sleep(6);
+        try {
+            demoService.getUuid(2);
+        } catch (Exception e) {
+
+        }
+        //into open
+    }
+
+    @Test
+    public void testHalfOpen2Close() throws InterruptedException {
+        for (int i = 0; i < 6; i++) {
+            try {
+                demoService.getUuid(2);
+            } catch (Exception e) {
+
+            }
+        }
+        //into half open
+        TimeUnit.SECONDS.sleep(6);
+        for (int i = 0; i < 6; i++) {
+            demoService.getUuid(1);
+        }
+        //into close
+    }
+
+    @Test
+    public void testNoTripException() {
+        for (int i = 0; i < 6; i++) {
+            try {
+                demoService.illegalEx(2);
+            } catch (Exception e) {
+
+            }
+        }
+        // still close
+        try {
+            demoService.illegalEx(2);
+        } catch (Exception e) {
+
+        }
+    }
+
 }
