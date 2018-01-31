7bbb804681d2c232e69b0436e1b29356e467601a
echo <18730214@qq.com>
2018-01-31 20:53:28 +0800

    更新说明文档

--- admin/src/main/java/org/ms/neural/admin/controller/WebController.java
+++ admin/src/main/java/org/ms/neural/admin/controller/WebController.java
@@ -2,15 +2,13 @@ package org.ms.neural.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
 import java.util.HashMap;
 
 @Controller
 public class WebController {
 
-    @RequestMapping("index")
+    @RequestMapping("/")
     public String index(HashMap<String, Object> map) {
         map.put("hello", "欢迎进入HTML页面");
         return "index";
