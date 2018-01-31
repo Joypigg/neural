6653a71fc8e36fb1426911cf4da963d7be95c68a
echo <18730214@qq.com>
2018-01-31 21:16:06 +0800

    更新页面

--- admin/src/main/java/org/ms/neural/admin/StaticConfig.java
+++ admin/src/main/java/org/ms/neural/admin/StaticConfig.java
@@ -1,16 +1,30 @@
 package org.ms.neural.admin;
 
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.Map;
+
+@Data
+@ToString
 @Configuration
+@ConfigurationProperties(prefix = "neural")
 public class StaticConfig extends WebMvcConfigurerAdapter {
+
+    public static final String CLASS_PATH = "classpath:";
+    private Map<String, String> resource;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        for (Map.Entry<String, String> entry : resource.entrySet()) {
+            registry.addResourceHandler(entry.getValue()).addResourceLocations(CLASS_PATH + entry.getKey());
+        }
+
         super.addResourceHandlers(registry);
     }
-    
+
 }
