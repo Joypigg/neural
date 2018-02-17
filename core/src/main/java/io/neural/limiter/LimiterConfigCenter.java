30a7538def2d431b9f1864eb279200ac136ef392
lry <595208882@qq.com>
2018-02-17 09:40:51 +0800

    Rename

--- core/src/main/java/io/neural/limiter/LimiterConfigCenter.java
+++ core/src/main/java/io/neural/limiter/LimiterConfigCenter.java
@@ -7,6 +7,7 @@ import io.neural.extension.Extension;
 import io.neural.limiter.LimiterConfig.Config;
 import io.neural.limiter.LimiterConfig.GlobalConfig;
 import io.neural.limiter.LimiterConfig.EventType;
+import io.neural.limiter.support.ILimiter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
