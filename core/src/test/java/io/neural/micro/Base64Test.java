ba6d65ff2b8cfbde7ea9557041a84c8c6ec49061
lry <595208882@qq.com>
2018-02-27 22:02:49 +0800

    remove Base64

--- core/src/test/java/io/neural/micro/Base64Test.java
+++
@@ -1,9 +0,0 @@
-package io.neural.micro;
-
-public class Base64Test {
-
-	public static void main(String[] args) {
-		System.out.println(Base64.getUrlEncoder().encodeToString("测试报文".getBytes()));
-	}
-	
-}
